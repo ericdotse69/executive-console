c8ebc8781ab1f6d2acaa2374d10dc2ef116674b8
ericdotse69 <ericd.dotse@coderaccoon.com.com>
2026-03-24 09:37:53 +0000

    Initiated PWA Protocol for native mobile installation

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'zone69-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install the Service Worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached file if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
